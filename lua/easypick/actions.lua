20fecf6bdb7d0aea80cffc6cddad344993d72b1f
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-12-28 12:44:35 +0100

    remove unused function

--- lua/easypick/actions.lua
+++ lua/easypick/actions.lua
@@ -1,20 +1,12 @@
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
+-- deprecated, use nvim_command
 local function run_nvim_command(prompt_bufnr, _)
 	actions.select_default:replace(function()
 		actions.close(prompt_bufnr)
 		local selection = action_state.get_selected_entry()
 		vim.cmd(selection[1])
-	end)
-	return true
-end
-
-local function run_easypick(prompt_bufnr, _)
-	actions.select_default:replace(function()
-		actions.close(prompt_bufnr)
-		local selection = action_state.get_selected_entry()
-		vim.cmd('Easypick ' .. selection[1])
 	end)
 	return true
 end
@@ -38,6 +30,5 @@ end
 
 return {
 	run_nvim_command = run_nvim_command,
-	run_easypick = run_easypick,
 	nvim_command = nvim_command
 }
