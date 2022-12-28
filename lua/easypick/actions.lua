d7a592f25061e63ead85a934c392581e8680d4bc
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-12-28 12:26:25 +0100

    create action to run prepended commands

--- lua/easypick/actions.lua
+++ lua/easypick/actions.lua
@@ -2,24 +2,41 @@ local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
 local function run_nvim_command(prompt_bufnr, _)
-  actions.select_default:replace(function()
-    actions.close(prompt_bufnr)
-    local selection = action_state.get_selected_entry()
-    vim.cmd(selection[1])
-  end)
-  return true
+	actions.select_default:replace(function()
+		actions.close(prompt_bufnr)
+		local selection = action_state.get_selected_entry()
+		vim.cmd(selection[1])
+	end)
+	return true
 end
 
 local function run_easypick(prompt_bufnr, _)
-  actions.select_default:replace(function()
-    actions.close(prompt_bufnr)
-    local selection = action_state.get_selected_entry()
-    vim.cmd('Easypick ' .. selection[1])
-  end)
-  return true
+	actions.select_default:replace(function()
+		actions.close(prompt_bufnr)
+		local selection = action_state.get_selected_entry()
+		vim.cmd('Easypick ' .. selection[1])
+	end)
+	return true
+end
+
+local function run_command_prepended(prefix)
+	if prefix == nil then
+		prefix = ''
+	end
+
+	return function (prompt_bufnr, _)
+		actions.select_default:replace(function()
+			actions.close(prompt_bufnr)
+			local selection = action_state.get_selected_entry()
+			vim.cmd('prefix' .. ' ' .. selection[1])
+		end)
+		return true
+
+	end
 end
 
 return {
 	run_nvim_command = run_nvim_command,
-	run_easypick = run_easypick
+	run_easypick = run_easypick,
+	run_command_prepended = run_command_prepended
 }
