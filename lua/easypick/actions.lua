435173007287e12eb36756efb659e852b38ea043
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-10-10 17:06:58 +0200

    introduce nvim_commandf

--- lua/easypick/actions.lua
+++ lua/easypick/actions.lua
@@ -1,7 +1,6 @@
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
--- deprecated, use nvim_command
 local function run_nvim_command(prompt_bufnr, _)
 	actions.select_default:replace(function()
 		actions.close(prompt_bufnr)
@@ -28,7 +27,21 @@ local function nvim_command(prefix)
 	end
 end
 
+local function nvim_commandf(template)
+	return function(prompt_bufnr, _)
+		actions.select_default:replace(function()
+			actions.close(prompt_bufnr)
+			local selection = action_state.get_selected_entry()
+			vim.cmd(string.format(template, selection[1]))
+		end)
+		return true
+	end
+end
+
 return {
+	-- deprecated, use nvim_commandf
 	run_nvim_command = run_nvim_command,
-	nvim_command = nvim_command
+	-- deprecated, use nvim_commandf
+	nvim_command = nvim_command,
+	nvim_commandf = nvim_commandf
 }
